c1e6eb1bed9b8eae2cac85090f72231b6f5f0496
David Hofer <chassis.poppets0x@icloud.com>
2024-05-22 17:10:59 +0200

    Added Endscreen

--- data.js
+++ data.js
@@ -7,44 +7,44 @@ let questions = [
         answer4: "Wrong3", 
         rightAnswer: 1
     },
-    {
-        question: "Question Two",
-        answer1: "Wrong1",
-        answer2: "Right",
-        answer3: "Wrong2",
-        answer4: "Wrong3", 
-        rightAnswer: 2
-    },
-    {
-        question: "Question Three",
-        answer1: "Wrong1",
-        answer2: "Wrong2",
-        answer3: "Right",
-        answer4: "Wrong3", 
-        rightAnswer: 3
-    },
-    {
-        question: "Question Four",
-        answer1: "Wrong1",
-        answer2: "Wrong2",
-        answer3: "Wrong3",
-        answer4: "Right", 
-        rightAnswer: 4
-    },
-    {
-        question: "Question Five",
-        answer1: "Right",
-        answer2: "Wrong1",
-        answer3: "Wrong2",
-        answer4: "Wrong3", 
-        rightAnswer: 1
-    },
-    {
-        question: "Question Six",
-        answer1: "Right",
-        answer2: "Wrong1",
-        answer3: "Wrong2",
-        answer4: "Wrong3", 
-        rightAnswer: 1
-    }
+    // {
+    //     question: "Question Two",
+    //     answer1: "Wrong1",
+    //     answer2: "Right",
+    //     answer3: "Wrong2",
+    //     answer4: "Wrong3", 
+    //     rightAnswer: 2
+    // },
+    // {
+    //     question: "Question Three",
+    //     answer1: "Wrong1",
+    //     answer2: "Wrong2",
+    //     answer3: "Right",
+    //     answer4: "Wrong3", 
+    //     rightAnswer: 3
+    // },
+    // {
+    //     question: "Question Four",
+    //     answer1: "Wrong1",
+    //     answer2: "Wrong2",
+    //     answer3: "Wrong3",
+    //     answer4: "Right", 
+    //     rightAnswer: 4
+    // },
+    // {
+    //     question: "Question Five",
+    //     answer1: "Right",
+    //     answer2: "Wrong1",
+    //     answer3: "Wrong2",
+    //     answer4: "Wrong3", 
+    //     rightAnswer: 1
+    // },
+    // {
+    //     question: "Question Six",
+    //     answer1: "Right",
+    //     answer2: "Wrong1",
+    //     answer3: "Wrong2",
+    //     answer4: "Wrong3", 
+    //     rightAnswer: 1
+    // }
 ]
--- script.js
+++ script.js
@@ -1,35 +1,71 @@
-
 let currentQuestion = 0;
 
 
 function init() {
-    document.getElementById("questionAmount").innerHTML = questions.length;
+  document.getElementById("questionAmount").innerHTML = questions.length;
 
-    showQuestion();
+  showQuestion();
 }
 
 
 function showQuestion() {
+
+  if ((currentQuestion >= questions.length)) {
+    document.getElementById("questionBody").style = 'display: none';
+    document.getElementById("endScreen").style = "";
+  } else {
     let question = questions[currentQuestion];
+
+    document.getElementById("currentQuestionAmount").innerHTML =currentQuestion + 1;
+
     document.getElementById("questionText").innerHTML = question["question"];
     document.getElementById("answer1").innerHTML = question["answer1"];
     document.getElementById("answer2").innerHTML = question["answer2"];
     document.getElementById("answer3").innerHTML = question["answer3"];
     document.getElementById("answer4").innerHTML = question["answer4"];
+  }
 }
 
 
 function answer(clickedAnswer) {
-    let question = questions[currentQuestion];
-    let clickedAnswerNumber = Number(clickedAnswer.slice(-1));
+  let question = questions[currentQuestion];
+  let clickedAnswerNumber = Number(clickedAnswer.slice(-1));
 
-    let idOfRightAnswer = `answer${question["rightAnswer"]}`;
+  let idOfRightAnswer = `answer${question["rightAnswer"]}`;
 
-    if (question["rightAnswer"] === clickedAnswerNumber) {
-        document.getElementById(clickedAnswer).parentNode.classList.add("bg-success");
-    } else {
-        document.getElementById(clickedAnswer).parentNode.classList.add("bg-danger");
-        document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
-    }
-    document.getElementById("nextButton").disabled = false;
+  if (question["rightAnswer"] === clickedAnswerNumber) {
+    document
+      .getElementById(clickedAnswer)
+      .parentNode.classList.add("bg-success");
+  } else {
+    document
+      .getElementById(clickedAnswer)
+      .parentNode.classList.add("bg-danger");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
+  }
+  document.getElementById("nextButton").disabled = false;
+}
+
+
+function nextQuestion() {
+  currentQuestion++;
+
+  document.getElementById("nextButton").disabled = true;
+
+  resetAnswerButtons();
+  showQuestion();
+}
+
+
+function resetAnswerButtons() {
+  document.getElementById("answer1").parentNode.classList.remove("bg-success");
+  document.getElementById("answer1").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer2").parentNode.classList.remove("bg-success");
+  document.getElementById("answer2").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer3").parentNode.classList.remove("bg-success");
+  document.getElementById("answer3").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer4").parentNode.classList.remove("bg-success");
+  document.getElementById("answer4").parentNode.classList.remove("bg-danger");
 }
